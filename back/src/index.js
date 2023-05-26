c1cd80064d8362a3b337c00f6cf4682ccb68e7a2
alescrocaro <Alexandre A. Scrocaro Junior>
2023-05-25 21:18:33 -0300

    Update: backend start code

--- back/src/index.js
+++ back/src/index.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const cors = require('cors');
-const routes  = require('./routes');
+const { Pool } = require('pg');
+const routes = require('./routes');
 
 const app = express();
+const pool = new Pool({
+  user: process.env.PGUSER,
+  host: process.env.PGHOST,
+  database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
+  port: process.env.PGPORT
+});
 
 app.use(cors());
 app.use(express.json());
+app.use((req, res, next) => {
+  req.db = pool; // adicionando a instância do pool ao objeto request para facilitar o acesso ao banco de dados nas rotas
+  next();
+});
 app.use(routes);
 
 app.listen(3333);
