6c78811765fe83e29fcdb2c822e97055b3fb9e82
alescrocaro <alescrocaro@gmail.com>
2023-06-13 10:00:23 -0300

    Update: vehicles service

--- back/src/service/vehicle.js
+++ back/src/service/vehicle.js
@@ -4,22 +4,35 @@ const { createClient } = require('@supabase/supabase-js');
 
 const supabase = createClient(process.env.SUPABASEURL, process.env.SUPABASEKEY);
 
-async function createVehicle(id) {
-    const { data, error } = await supabase.from('VEHICLE').insert([{ id, placa: 'TESTE2' },])
-    console.log(data);
-    console.log(error);
-    return data
-}
+async function createVehicle (data) {
+    return await supabase
+        .from('VEHICLE')
+        .insert([data]);
+};
 
-async function getVehicle(id) {
-    const { data, error } = await supabase.from('VEHICLE').select('*');
-    console.log(data)
-    return data;
-}
+async function getVehicles () {
+    return await supabase
+        .from('VEHICLE')
+        .select('*');
+};
 
+async function updateVehicle (id, data) {
+    return await supabase
+        .from('VEHICLE')
+        .update(data)
+        .eq('id', id)
+};
 
+async function deleteVehicle (id) {
+    return await supabase
+        .from('VEHICLE')
+        .delete()
+        .eq('id', id)
+};
 
 module.exports = {
-    getVehicle,
     createVehicle,
+    getVehicles,
+    updateVehicle,
+    deleteVehicle,
 }
