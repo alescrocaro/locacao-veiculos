b685c3fad7021397ed5688be5f916a2979a9443c
alescrocaro <alescrocaro@gmail.com>
2023-06-13 12:52:41 -0300

    Add: user CRUD routes

--- back/src/routes/routes.js
+++ back/src/routes/routes.js
@@ -3,13 +3,25 @@ const express = require('express');
 const routes = express.Router();
 
 const VehiclesController = require('../controllers/VehiclesController');
+const UsersController = require('../controllers/UsersController');
 
+// VEHICLE
 routes.post('/vehicles', VehiclesController.createVehicle);
 routes.get('/vehicles', VehiclesController.index);
 routes.put('/vehicles/:id', VehiclesController.updateVehicle)
 routes.delete('/vehicles/:id', VehiclesController.deleteVehicle)
 
 routes.get('/vehicles/:id', VehiclesController.getVehicle);
+
+
+// USER 
+routes.post('/users', UsersController.createUser);
+routes.get('/users', UsersController.index);
+routes.put('/users/:id', UsersController.updateUser)
+routes.delete('/users/:id', UsersController.deleteUser)
+
+routes.get('/users/:id', UsersController.getUser);
+
 
 
 module.exports = routes;
