5b790c80127c1a0499db16998e60cc2474a1c9e6
alescrocaro <alescrocaro@gmail.com>
2023-06-13 09:05:20 -0300

    Add: createVehicle route

--- back/src/routes/routes.js
+++ back/src/routes/routes.js
@@ -5,6 +5,7 @@ const routes = express.Router();
 const VehiclesController = require('../controllers/VehiclesController');
 
 routes.get('/vehicles', VehiclesController.index);
+routes.post('/createVehicle', VehiclesController.create);
 
 
 
