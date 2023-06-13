dae6266b42c62142615da3a6fde7c854674ed777
alescrocaro <alescrocaro@gmail.com>
2023-06-13 09:06:12 -0300

    Add: create vehicle to controller

--- back/src/controllers/VehiclesController.js
+++ back/src/controllers/VehiclesController.js
@@ -1,11 +1,19 @@
-const connection = require('../service/user');
+const connection = require('../service/vehicle');
 
 module.exports = {
   async index(req, res) {
     // const ong_id  = req.headers.authorization;
-    const id  = 1;
+    const id = 1;
 
     const vehicle = await connection.getVehicle(id)
+
+    return res.json(vehicle);
+  },
+
+  async create(req, res) {
+    const id = 2;
+
+    const vehicle = await connection.createVehicle(id)
 
     return res.json(vehicle);
   }
