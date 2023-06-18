a971365138b6942c48ddb2226d387f8691a22342
alescrocaro <Alexandre A. Scrocaro Junior>
2023-06-18 20:43:02 -0300

    Add: Home page

---
+++ front/src/pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import { Button } from "antd";
+import Layout from "../../components/Layout/Layout";
+
+const Home = () => {
+	return (
+		<Layout>
+			<h1>HOME</h1>
+			{/* <Button href="/login">LOGAR</Button> */}
+		</Layout>
+	)
+}
+
+export default Home;
