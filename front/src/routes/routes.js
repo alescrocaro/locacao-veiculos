702d91d7556d351f1841c7946b7844c6dfcf82e5
alescrocaro <Alexandre A. Scrocaro Junior>
2023-06-18 20:42:26 -0300

    Add: routes management

---
+++ front/src/routes/routes.js
@@ -0,0 +1,29 @@
+import { useNavigate, Route, Routes } from "react-router-dom";
+import Login from '../pages/Login/Login';
+import Home from '../pages/Home/Home';
+import { useToken } from "../context/AuthContext";
+import { useEffect } from "react";
+
+const RoutesProvider = () => {
+  const { authenticated } = useToken();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!authenticated){
+      navigate('/login');
+    } else {
+      navigate('/')
+    }
+  }, [authenticated]);
+
+  return (
+
+    <Routes>
+    <Route exact path="/" element={<Home />} />
+    <Route exact path="/login" element={<Login />} />
+    {/* <Route exact path="/*" element={<NotFound />} /> */}
+  </Routes>
+  );
+}
+
+export default RoutesProvider;
