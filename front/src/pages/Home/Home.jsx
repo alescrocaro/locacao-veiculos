b0c9d8dbe9b9059cd9a29781e3a7ff1525b968b8
alescrocaro <Alexandre A. Scrocaro Junior>
2023-06-20 21:15:24 -0300

    Add vehicles listing to home page

--- front/src/pages/Home/Home.jsx
+++ front/src/pages/Home/Home.jsx
@@ -1,11 +1,43 @@
-import { Button } from "antd";
+import { useEffect, useState } from "react";
+import { Card, notification } from "antd";
 import Layout from "../../components/Layout/Layout";
+import { useToken } from "../../context/AuthContext";
+import VehiclesTable from "./VehiclesTable";
+import { api } from "../../services/api";
+
 
 const Home = () => {
+	const [vehicles, setVehicles] = useState([]);
+	const [isLoading, setIsLoading] = useState([]);
+	const { user } = useToken();
+	console.log(user);
+
+	useEffect(() => {
+		const fetchVehicles = async () => {
+			return await api.get('/vehicles');
+		}
+		setIsLoading(true);
+		fetchVehicles()
+			.then(({ data }) => {
+				setVehicles(data.data);
+			})
+			.catch(err => {
+				notification.error({
+					message: 'Erro ao listar veículos',
+				});
+			})
+			.finally(() => {
+				setIsLoading(false);
+			});
+	}, []);
+
 	return (
 		<Layout>
-			<h1>HOME</h1>
-			{/* <Button href="/login">LOGAR</Button> */}
+			<Card 
+				title="Veículos"
+			>
+				<VehiclesTable vehicles={vehicles} loading={isLoading} />
+			</Card>
 		</Layout>
 	)
 }
